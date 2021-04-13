9b57ca7c9e17c1fbe2fde92a83a8bca05a2226d9
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-04-13 02:13:54 -0500

    Clickgui basics

---
+++ src/main/java/dev/tensor/misc/gui/ClickGui.java
@@ -0,0 +1,54 @@
+package dev.tensor.misc.gui;
+
+import dev.tensor.Tensor;
+import dev.tensor.misc.imp.Wrapper;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.LiteralText;
+
+/**
+ * @author IUDevman
+ * @since 04-13-2021
+ */
+
+public final class ClickGui extends Screen implements Wrapper {
+
+    protected ClickGui() {
+        super(new LiteralText(Tensor.MOD_NAME + " ClickGui"));
+    }
+
+    @Override
+    public void onClose() {
+        super.onClose();
+    }
+
+    @Override
+    public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+
+    }
+
+    @Override
+    public boolean mouseScrolled(double mouseX, double mouseY, double scroll) {
+        return true;
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int clickedButton) {
+        return true;
+    }
+
+    @Override
+    public boolean mouseReleased(double mouseX, double mouseY, int releasedButton) {
+        return true;
+    }
+
+    @Override
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+        return true;
+    }
+
+    @Override
+    public boolean isPauseScreen() {
+        return false;
+    }
+}
