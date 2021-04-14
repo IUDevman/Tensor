6c8d78933975170ec345cda52e681667e6c7bac1
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-04-14 05:01:37 -0500

    Added sprint

---
+++ src/main/java/dev/tensor/feature/modules/Sprint.java
@@ -0,0 +1,31 @@
+package dev.tensor.feature.modules;
+
+import dev.tensor.misc.imp.Category;
+import dev.tensor.misc.imp.Module;
+import dev.tensor.misc.imp.settings.BooleanSetting;
+import org.lwjgl.glfw.GLFW;
+
+/**
+ * @author IUDevman
+ * @since 04-14-2021
+ */
+
+@Module.Info(name = "Sprint", category = Category.Movement, bind = GLFW.GLFW_KEY_U, messages = true)
+public final class Sprint extends Module {
+
+    BooleanSetting stationary = new BooleanSetting("Stationary", false);
+    BooleanSetting reverse = new BooleanSetting("Reverse", true);
+    BooleanSetting sideways = new BooleanSetting("Sideways", true);
+
+    public void onDisable() {
+        getPlayer().setSprinting(false);
+    }
+
+    public void onTick() {
+        if (!stationary.getValue() && getPlayer().forwardSpeed == 0 && getPlayer().sidewaysSpeed == 0) return;
+        else if (!sideways.getValue() && getPlayer().sidewaysSpeed != 0) return;
+        else if (!reverse.getValue() && getPlayer().forwardSpeed < 0) return;
+
+        getPlayer().setSprinting(true);
+    }
+}
