c4be2671d508142bfd65a99f190148402386f989
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-04-17 02:22:25 -0500

    Added Toggle Module

---
+++ src/main/java/dev/tensor/feature/commands/Toggle.java
@@ -0,0 +1,52 @@
+package dev.tensor.feature.commands;
+
+import dev.tensor.feature.managers.ModuleManager;
+import dev.tensor.misc.imp.Command;
+import dev.tensor.misc.imp.Module;
+import dev.tensor.misc.util.MessageUtil;
+import net.minecraft.util.Formatting;
+
+/**
+ * @author IUDevman
+ * @since 04-17-2021
+ */
+
+public final class Toggle implements Command {
+
+    @Override
+    public String getName() {
+        return "Toggle";
+    }
+
+    @Override
+    public String getSyntax() {
+        return "{alias} [module]";
+    }
+
+    @Override
+    public String[] getAliases() {
+        return new String[] {
+                "toggle",
+                "enable",
+                "disable",
+                "togglemodule"
+        };
+    }
+
+    @Override
+    public void onCommand(String[] message) {
+        String moduleName = message[1];
+
+        Module module = ModuleManager.INSTANCE.getModule(moduleName);
+
+        if (module == null) {
+            MessageUtil.INSTANCE.sendClientMessage("Invalid Module (" + Formatting.YELLOW + moduleName + Formatting.GRAY + ")!", true, true);
+            return;
+        }
+
+        module.toggle();
+
+        String value = module.isEnabled() ? Formatting.GREEN + "true" : Formatting.RED + "false";
+        MessageUtil.INSTANCE.sendClientMessage("Toggled " + module.getName() + " (" + value + Formatting.GRAY + ")!", true, true);
+    }
+}
