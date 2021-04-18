06a7ea61928c23c90c0dc79093f583fd6c01ae9d
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-04-18 09:13:05 -0500

    Liquids option for Sprint

--- src/main/java/dev/tensor/feature/modules/Sprint.java
+++ src/main/java/dev/tensor/feature/modules/Sprint.java
@@ -16,6 +16,7 @@ public final class Sprint extends Module {
     public final BooleanSetting stationary = new BooleanSetting("Stationary", false);
     public final BooleanSetting reverse = new BooleanSetting("Reverse", true);
     public final BooleanSetting sideways = new BooleanSetting("Sideways", true);
+    public final BooleanSetting liquids = new BooleanSetting("Liquids", false);
 
     public void onDisable() {
         getPlayer().setSprinting(false);
@@ -25,6 +26,7 @@ public final class Sprint extends Module {
         if (!stationary.getValue() && getPlayer().forwardSpeed == 0 && getPlayer().sidewaysSpeed == 0) return;
         else if (!sideways.getValue() && getPlayer().sidewaysSpeed != 0) return;
         else if (!reverse.getValue() && getPlayer().forwardSpeed < 0) return;
+        else if (!liquids.getValue() && (getPlayer().isInsideWaterOrBubbleColumn() || getPlayer().isInLava())) return;
 
         getPlayer().setSprinting(true);
     }
