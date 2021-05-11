9da24720ecb0376022f9e86b37342470bf3cb0ea
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-05-10 20:33:12 -0500

    Added AutoRespawn

---
+++ src/main/java/dev/tensor/feature/modules/AutoRespawn.java
@@ -0,0 +1,21 @@
+package dev.tensor.feature.modules;
+
+import dev.tensor.misc.imp.Category;
+import dev.tensor.misc.imp.Module;
+import net.minecraft.client.gui.screen.DeathScreen;
+
+/**
+ * @author IUDevman
+ * @since 05-10-2021
+ */
+
+@Module.Info(name = "AutoRespawn", category = Category.Misc)
+public final class AutoRespawn extends Module {
+
+    public void onTick() {
+        if (getMinecraft().currentScreen instanceof DeathScreen) {
+            getPlayer().requestRespawn();
+            getMinecraft().openScreen(null);
+        }
+    }
+}
