f47d23c0f89781f3e5f412dd63099e6ef93a9717
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-05-11 15:25:23 -0500

    NoBreakDelay

--- src/main/java/dev/tensor/backend/mixins/ClientPlayerInteractionManagerMixin.java
+++ src/main/java/dev/tensor/backend/mixins/ClientPlayerInteractionManagerMixin.java
@@ -3,12 +3,14 @@ package dev.tensor.backend.mixins;
 import dev.tensor.Tensor;
 import dev.tensor.backend.events.BlockInteractEvent;
 import dev.tensor.feature.managers.ModuleManager;
+import dev.tensor.feature.modules.NoBreakDelay;
 import dev.tensor.feature.modules.Reach;
 import dev.tensor.misc.imp.Wrapper;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -21,9 +23,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ClientPlayerInteractionManager.class)
 public final class ClientPlayerInteractionManagerMixin implements Wrapper {
 
+    @Shadow private int blockBreakingCooldown;
+
     @Inject(method = "updateBlockBreakingProgress", at = @At("HEAD"))
     public void updateBlockBreakingProgress(BlockPos pos, Direction direction, CallbackInfoReturnable<Boolean> cir) {
         Tensor.INSTANCE.EVENT_BUS.post(new BlockInteractEvent(BlockInteractEvent.Type.Damage, pos));
+
+        NoBreakDelay noBreakDelay = ModuleManager.INSTANCE.getModule(NoBreakDelay.class);
+
+        if (noBreakDelay.isEnabled()) blockBreakingCooldown = 0;
     }
 
     @Inject(method = "breakBlock", at = @At("HEAD"))
---
+++ src/main/java/dev/tensor/feature/modules/NoBreakDelay.java
@@ -0,0 +1,14 @@
+package dev.tensor.feature.modules;
+
+import dev.tensor.misc.imp.Category;
+import dev.tensor.misc.imp.Module;
+
+/**
+ * @author IUDevman
+ * @since 05-11-2021
+ */
+
+@Module.Info(name = "NoBreakDelay", category = Category.Player)
+public final class NoBreakDelay extends Module {
+
+}
