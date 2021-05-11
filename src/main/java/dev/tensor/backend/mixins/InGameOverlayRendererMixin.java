d7c7dd90e6d1df13ddddaa1ed2933d9d11bfe2de
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-05-10 20:50:39 -0500

    No Overlay

---
+++ src/main/java/dev/tensor/backend/mixins/InGameOverlayRendererMixin.java
@@ -0,0 +1,28 @@
+package dev.tensor.backend.mixins;
+
+import dev.tensor.feature.managers.ModuleManager;
+import dev.tensor.feature.modules.NoOverlay;
+import dev.tensor.misc.imp.Wrapper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.InGameOverlayRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * @author IUDevman
+ * @since 05-10-2021
+ */
+
+@Mixin(InGameOverlayRenderer.class)
+public final class InGameOverlayRendererMixin implements Wrapper {
+    
+    @Inject(method = "renderOverlays", at = @At("HEAD"), cancellable = true)
+    private static void renderOverlays(MinecraftClient minecraftClient, MatrixStack matrixStack, CallbackInfo callbackInfo) {
+        NoOverlay noOverlay = ModuleManager.INSTANCE.getModule(NoOverlay.class);
+
+        if (noOverlay.isEnabled()) callbackInfo.cancel();
+    }
+}
---
+++ src/main/java/dev/tensor/feature/modules/NoOverlay.java
@@ -0,0 +1,14 @@
+package dev.tensor.feature.modules;
+
+import dev.tensor.misc.imp.Category;
+import dev.tensor.misc.imp.Module;
+
+/**
+ * @author IUDevman
+ * @since 05-10-2021
+ */
+
+@Module.Info(name = "NoOverlay", category = Category.Render)
+public final class NoOverlay extends Module {
+
+}
