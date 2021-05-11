c9b8526b2ac78e4447ddf2ce6e6369f88932df4b
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-05-11 11:23:58 -0500

    Update InGameHudMixin.java

--- src/main/java/dev/tensor/backend/mixins/InGameHudMixin.java
+++ src/main/java/dev/tensor/backend/mixins/InGameHudMixin.java
@@ -5,6 +5,7 @@ import dev.tensor.feature.modules.NoOverlay;
 import dev.tensor.misc.imp.Wrapper;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -27,6 +28,13 @@ public final class InGameHudMixin implements Wrapper {
 
     @Inject(method = "renderStatusEffectOverlay", at = @At("HEAD"), cancellable = true)
     public void renderStatusEffectOverlay(MatrixStack matrices, CallbackInfo callbackInfo) {
+        NoOverlay noOverlay = ModuleManager.INSTANCE.getModule(NoOverlay.class);
+
+        if (noOverlay.isEnabled()) callbackInfo.cancel();
+    }
+
+    @Inject(method = "renderVignetteOverlay", at = @At("HEAD"), cancellable = true)
+    public void renderVignetteOverlay(Entity entity, CallbackInfo callbackInfo) {
         NoOverlay noOverlay = ModuleManager.INSTANCE.getModule(NoOverlay.class);
 
         if (noOverlay.isEnabled()) callbackInfo.cancel();
