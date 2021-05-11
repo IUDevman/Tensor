ba9c75ceff8e5cefc21154f17d62efca72aadce9
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-05-11 00:41:59 -0500

    Update InGameHudMixin.java

--- src/main/java/dev/tensor/backend/mixins/InGameHudMixin.java
+++ src/main/java/dev/tensor/backend/mixins/InGameHudMixin.java
@@ -4,6 +4,7 @@ import dev.tensor.feature.managers.ModuleManager;
 import dev.tensor.feature.modules.NoOverlay;
 import dev.tensor.misc.imp.Wrapper;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,6 +20,13 @@ public final class InGameHudMixin implements Wrapper {
 
     @Inject(method = "renderPumpkinOverlay", at = @At("HEAD"), cancellable = true)
     public void renderPumpkinOverlay(CallbackInfo callbackInfo) {
+        NoOverlay noOverlay = ModuleManager.INSTANCE.getModule(NoOverlay.class);
+
+        if (noOverlay.isEnabled()) callbackInfo.cancel();
+    }
+
+    @Inject(method = "renderStatusEffectOverlay", at = @At("HEAD"), cancellable = true)
+    public void renderStatusEffectOverlay(MatrixStack matrices, CallbackInfo callbackInfo) {
         NoOverlay noOverlay = ModuleManager.INSTANCE.getModule(NoOverlay.class);
 
         if (noOverlay.isEnabled()) callbackInfo.cancel();
