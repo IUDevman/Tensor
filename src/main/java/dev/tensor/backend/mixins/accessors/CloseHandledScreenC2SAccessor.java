ea1ae64007a0853d8d263523c7c32bc6734596a7
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-05-12 12:02:57 -0500

    Added XCarry

---
+++ src/main/java/dev/tensor/backend/mixins/accessors/CloseHandledScreenC2SAccessor.java
@@ -0,0 +1,12 @@
+package dev.tensor.backend.mixins.accessors;
+
+import net.minecraft.network.packet.c2s.play.CloseHandledScreenC2SPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(CloseHandledScreenC2SPacket.class)
+public interface CloseHandledScreenC2SAccessor {
+
+    @Accessor("syncId")
+    int getSyncId();
+}
---
+++ src/main/java/dev/tensor/feature/modules/XCarry.java
@@ -0,0 +1,34 @@
+package dev.tensor.feature.modules;
+
+import dev.tensor.backend.events.PacketEvent;
+import dev.tensor.backend.mixins.accessors.CloseHandledScreenC2SAccessor;
+import dev.tensor.misc.imp.Category;
+import dev.tensor.misc.imp.Module;
+import me.zero.alpine.listener.EventHandler;
+import me.zero.alpine.listener.Listener;
+import net.minecraft.network.packet.c2s.play.CloseHandledScreenC2SPacket;
+
+@Module.Info(name = "XCarry", category = Category.Player)
+public final class XCarry extends Module {
+
+    private boolean cancelled = false;
+
+    public void onDisable() {
+        if (cancelled) getNetwork().sendPacket(new CloseHandledScreenC2SPacket(getPlayer().playerScreenHandler.syncId));
+    }
+
+    @SuppressWarnings("unused")
+    @EventHandler
+    private final Listener<PacketEvent> packetEventListener = new Listener<>(event -> {
+        if (event.getType() != PacketEvent.Type.Send) return;
+
+        if (event.getPacket() instanceof CloseHandledScreenC2SPacket) {
+            CloseHandledScreenC2SPacket packet = (CloseHandledScreenC2SPacket) event.getPacket();
+
+            if (((CloseHandledScreenC2SAccessor) packet).getSyncId() == getPlayer().playerScreenHandler.syncId) {
+                event.cancel();
+                cancelled = true;
+            }
+        }
+    });
+}
