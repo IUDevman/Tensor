1065ba76614c5ee938c6bdee766cbfc100712aa5
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-05-12 01:14:26 -0500

    Weather sound cancelling

--- src/main/java/dev/tensor/feature/modules/NoWeather.java
+++ src/main/java/dev/tensor/feature/modules/NoWeather.java
@@ -1,7 +1,12 @@
 package dev.tensor.feature.modules;
 
+import dev.tensor.backend.events.PacketEvent;
 import dev.tensor.misc.imp.Category;
 import dev.tensor.misc.imp.Module;
+import me.zero.alpine.listener.EventHandler;
+import me.zero.alpine.listener.Listener;
+import net.minecraft.network.packet.s2c.play.PlaySoundS2CPacket;
+import net.minecraft.sound.SoundEvents;
 
 /**
  * @author IUDevman
@@ -11,4 +16,16 @@ import dev.tensor.misc.imp.Module;
 @Module.Info(name = "NoWeather", category = Category.Render)
 public final class NoWeather extends Module {
 
+    @SuppressWarnings("unused")
+    @EventHandler
+    private final Listener<PacketEvent> packetEventListener = new Listener<>(event -> {
+        if (event.getType() != PacketEvent.Type.Receive) return;
+
+        if (event.getPacket() instanceof PlaySoundS2CPacket) {
+            PlaySoundS2CPacket packet = (PlaySoundS2CPacket) event.getPacket();
+
+            if (packet.getSound().equals(SoundEvents.ENTITY_LIGHTNING_BOLT_THUNDER)) event.cancel();
+            else if (packet.getSound().equals(SoundEvents.WEATHER_RAIN) || packet.getSound().equals(SoundEvents.WEATHER_RAIN_ABOVE)) event.cancel();
+        }
+    });
 }
