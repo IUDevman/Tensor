baf8c9e643b7e581df572003059cd2d62eae3f97
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-05-13 00:27:44 -0500

    Added MiddleClickPearl

---
+++ src/main/java/dev/tensor/feature/modules/MiddleClickPearl.java
@@ -0,0 +1,37 @@
+package dev.tensor.feature.modules;
+
+import dev.tensor.backend.events.KeyPressedEvent;
+import dev.tensor.misc.imp.Category;
+import dev.tensor.misc.imp.Module;
+import dev.tensor.misc.util.InventoryUtil;
+import me.zero.alpine.listener.EventHandler;
+import me.zero.alpine.listener.Listener;
+import net.minecraft.item.Items;
+import net.minecraft.util.Hand;
+import org.lwjgl.glfw.GLFW;
+
+import java.util.Objects;
+
+/**
+ * @author IUDevman
+ * @since 05-13-2021
+ */
+
+@Module.Info(name = "MiddleClickPearl", category = Category.Player)
+public final class MiddleClickPearl extends Module {
+
+    @SuppressWarnings("unused")
+    @EventHandler
+    private final Listener<KeyPressedEvent> keyPressedEventListener = new Listener<>(event -> {
+        if (event.getBind() != GLFW.GLFW_MOUSE_BUTTON_MIDDLE) return;
+
+        final int oldSlot = getInventory().selectedSlot;
+        final int newSlot = InventoryUtil.INSTANCE.findItem(Items.ENDER_PEARL);
+
+        if (newSlot != -1) {
+            InventoryUtil.INSTANCE.swap(newSlot);
+            Objects.requireNonNull(getMinecraft().interactionManager).interactItem(getPlayer(), getWorld(), Hand.MAIN_HAND);
+            InventoryUtil.INSTANCE.swap(oldSlot);
+        }
+    });
+}
