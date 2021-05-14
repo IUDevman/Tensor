70e428d0d21e8c5e4172f61900050635146abed9
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-05-14 00:21:44 -0500

    Added ChatSuffix

---
+++ src/main/java/dev/tensor/feature/modules/ChatSuffix.java
@@ -0,0 +1,51 @@
+package dev.tensor.feature.modules;
+
+import dev.tensor.Tensor;
+import dev.tensor.backend.events.PacketEvent;
+import dev.tensor.backend.mixins.accessors.ChatMessageC2SPacketAccessor;
+import dev.tensor.feature.managers.CommandManager;
+import dev.tensor.misc.imp.Category;
+import dev.tensor.misc.imp.Module;
+import dev.tensor.misc.imp.settings.EnumSetting;
+import me.zero.alpine.listener.EventHandler;
+import me.zero.alpine.listener.Listener;
+import net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket;
+
+@Module.Info(name = "ChatSuffix", category = Category.Misc)
+public final class ChatSuffix extends Module {
+
+    public final EnumSetting mode = new EnumSetting("Mode", Mode.Regular);
+
+    @SuppressWarnings("unused")
+    @EventHandler
+    private final Listener<PacketEvent> packetEventListener = new Listener<>(event -> {
+        if (event.getType() != PacketEvent.Type.Send) return;
+
+        if (event.getPacket() instanceof ChatMessageC2SPacket) {
+            ChatMessageC2SPacket packet = (ChatMessageC2SPacket) event.getPacket();
+
+            String message = packet.getChatMessage();
+
+            if (message.startsWith(CommandManager.INSTANCE.getPrefix()) || message.startsWith("/")) return;
+
+            ((ChatMessageC2SPacketAccessor) packet).setChatMessage(message + getSuffix());
+        }
+    });
+
+    private String getSuffix() {
+        switch ((Mode) mode.getValue()) {
+            case Script: {
+                return " \u23D0 \u1d1b\u1d07\u0274\ua731\u1d0f\u0280";
+            }
+            case Regular: {
+                return " | " + Tensor.INSTANCE.MOD_NAME;
+            }
+            default: return "You broke something!";
+        }
+    }
+
+    public enum Mode {
+        Script,
+        Regular
+    }
+}
