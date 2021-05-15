d70e749a57f5aa3371640d05fb4006e5f1f5ee8e
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-05-15 02:16:55 -0500

    Added Mega-WIP ArrayList

---
+++ src/main/java/dev/tensor/feature/modules/ArrayList.java
@@ -0,0 +1,33 @@
+package dev.tensor.feature.modules;
+
+import dev.tensor.feature.managers.ModuleManager;
+import dev.tensor.misc.imp.Category;
+import dev.tensor.misc.imp.Module;
+import dev.tensor.misc.imp.settings.NumberSetting;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.util.math.MatrixStack;
+
+import java.awt.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author IUDevman
+ * @since 05-15-2021
+ */
+
+//this is a mega WIP module
+@Module.Info(name = "ArrayList", category = Category.HUD)
+public final class ArrayList extends Module {
+
+    public final NumberSetting x = new NumberSetting("X Position", 0, 0, 1000, 0);
+    public final NumberSetting y = new NumberSetting("Y Position", 100, 0, 1000, 0);
+
+    public void onRender2D(MatrixStack matrixStack) {
+        AtomicInteger incrementY = new AtomicInteger();
+
+        ModuleManager.INSTANCE.getModules().forEach(module -> {
+            DrawableHelper.drawStringWithShadow(matrixStack, getMinecraft().textRenderer, module.getName(), this.x.getValue().intValue(), this.y.getValue().intValue() + incrementY.get(), new Color(255, 255, 255, 255).getRGB());
+            incrementY.getAndAdd(getMinecraft().textRenderer.fontHeight);
+        });
+    }
+}
