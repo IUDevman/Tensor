ff8797aee6a5f241854768caeb557a4d80b636f3
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-05-14 19:17:19 -0500

    Adjust min scaling

--- src/main/java/dev/tensor/feature/modules/ViewModel.java
+++ src/main/java/dev/tensor/feature/modules/ViewModel.java
@@ -16,5 +16,5 @@ public final class ViewModel extends Module {
     public final BooleanSetting renderEmptyMainHand = new BooleanSetting("Render Empty Mainhand", false);
     public final NumberSetting vertical = new NumberSetting("Vertical", 0.20, -2.00, 2.00, 2);
     public final NumberSetting horizontal = new NumberSetting("Horizontal", -1.20, -2.00, 2.00, 2);
-    public final NumberSetting scale = new NumberSetting("Scale", 1.00, 0.50, 1.50, 2);
+    public final NumberSetting scale = new NumberSetting("Scale", 1.00, 0.10, 1.50, 2);
 }
