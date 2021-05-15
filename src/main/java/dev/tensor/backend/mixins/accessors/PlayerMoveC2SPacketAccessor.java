c3beedb521e0af313acd10034000e778f4c13291
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-05-14 19:47:55 -0500

    Update PlayerMoveC2SPacketAccessor.java

--- src/main/java/dev/tensor/backend/mixins/accessors/PlayerMoveC2SPacketAccessor.java
+++ src/main/java/dev/tensor/backend/mixins/accessors/PlayerMoveC2SPacketAccessor.java
@@ -4,6 +4,11 @@ import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
+/**
+ * @author IUDevman
+ * @since 05-14-2021
+ */
+
 @Mixin(PlayerMoveC2SPacket.class)
 public interface PlayerMoveC2SPacketAccessor {
 
