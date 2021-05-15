f46ba4e518b127ef699772302d4b33f2e50ad819
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-05-14 19:42:02 -0500

    OnGround

---
+++ src/main/java/dev/tensor/backend/mixins/accessors/PlayerMoveC2SPacketAccessor.java
@@ -0,0 +1,12 @@
+package dev.tensor.backend.mixins.accessors;
+
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(PlayerMoveC2SPacket.class)
+public interface PlayerMoveC2SPacketAccessor {
+
+    @Accessor
+    void setOnGround(boolean onGround);
+}
---
+++ src/main/java/dev/tensor/feature/modules/OnGround.java
@@ -0,0 +1,30 @@
+package dev.tensor.feature.modules;
+
+import dev.tensor.backend.events.PacketEvent;
+import dev.tensor.backend.mixins.accessors.PlayerMoveC2SPacketAccessor;
+import dev.tensor.misc.imp.Category;
+import dev.tensor.misc.imp.Module;
+import me.zero.alpine.listener.EventHandler;
+import me.zero.alpine.listener.Listener;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+
+/**
+ * @author IUDevman
+ * @since 05-14-2021
+ */
+
+@Module.Info(name = "OnGround", category = Category.Player)
+public final class OnGround extends Module {
+
+    @SuppressWarnings("unused")
+    @EventHandler
+    private final Listener<PacketEvent> packetEventListener = new Listener<>(event -> {
+        if (event.getType() != PacketEvent.Type.Send) return;
+
+        if (event.getPacket() instanceof PlayerMoveC2SPacket) {
+            PlayerMoveC2SPacket packet = (PlayerMoveC2SPacket) event.getPacket();
+
+            ((PlayerMoveC2SPacketAccessor) packet).setOnGround(false);
+        }
+    });
+}
