0708586af4efd25a87fbf9567759dfd6c183cc9a
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-05-16 02:49:10 -0500

    Create Disconnect.java

---
+++ src/main/java/dev/tensor/feature/modules/Disconnect.java
@@ -0,0 +1,18 @@
+package dev.tensor.feature.modules;
+
+import dev.tensor.misc.imp.Category;
+import dev.tensor.misc.imp.Module;
+
+/**
+ * @author IUDevman
+ * @since 05-16-2021
+ */
+
+@Module.Info(name = "Disconnect", category = Category.Misc)
+public final class Disconnect extends Module {
+
+    public void onEnable() {
+        getWorld().disconnect();
+        disable();
+    }
+}
