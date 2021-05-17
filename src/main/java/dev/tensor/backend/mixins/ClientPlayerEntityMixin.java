4e6f3aa123523f877751252da620b42a579a8a06
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-05-16 19:33:19 -0500

    Update ClientPlayerEntityMixin.java

--- src/main/java/dev/tensor/backend/mixins/ClientPlayerEntityMixin.java
+++ src/main/java/dev/tensor/backend/mixins/ClientPlayerEntityMixin.java
@@ -7,10 +7,8 @@ import dev.tensor.feature.modules.NoSlow;
 import dev.tensor.misc.imp.Wrapper;
 import net.minecraft.client.network.ClientPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -21,8 +19,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ClientPlayerEntity.class)
 public abstract class ClientPlayerEntityMixin implements Wrapper {
-
-    @Shadow public abstract boolean isUsingItem();
 
     @Inject(method = "updateNausea", at = @At("HEAD"), cancellable = true)
     public void updateNausea(CallbackInfo callbackInfo) {
@@ -38,13 +34,14 @@ public abstract class ClientPlayerEntityMixin implements Wrapper {
         if (noSlow.isEnabled() && noSlow.sneaking.getValue()) cir.setReturnValue(false);
     }
 
-    @Redirect(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;isUsingItem()Z"))
-    public boolean tickMovement(ClientPlayerEntity clientPlayerEntity) {
+    @Inject(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;isUsingItem()Z"))
+    public void tickMovement(CallbackInfo callbackInfo) {
         NoSlow noSlow = ModuleManager.INSTANCE.getModule(NoSlow.class);
 
-        if (noSlow.isEnabled() && noSlow.items.getValue()) return false;
-
-        return isUsingItem();
+        if (noSlow.isEnabled() && noSlow.items.getValue() && getPlayer().isUsingItem()) {
+            getPlayer().input.movementForward /= 0.2F;
+            getPlayer().input.movementSideways /= 0.2F;
+        }
     }
 
     @Inject(method = "pushOutOfBlocks", at = @At("HEAD"), cancellable = true)
