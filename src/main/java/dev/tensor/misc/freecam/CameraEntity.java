42e4bbc7daf08853ad818f79a7fc9f935ca1c18f
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-05-18 14:10:11 -0500

    Update CameraEntity.java

--- src/main/java/dev/tensor/misc/freecam/CameraEntity.java
+++ src/main/java/dev/tensor/misc/freecam/CameraEntity.java
@@ -54,17 +54,17 @@ public final class CameraEntity extends OtherClientPlayerEntity implements Wrapp
     public void tickMovement() {
         this.setVelocity(0, 0, 0);
 
-        input.tick(false);
+        this.input.tick(false);
 
-        float upDown = (this.input.sneaking ? -freecam.getSpeed() : 0) + (this.input.jumping ? freecam.getSpeed() : 0);
+        float upDown = (this.input.sneaking ? -this.freecam.getSpeed() : 0) + (this.input.jumping ? this.freecam.getSpeed() : 0);
 
-        Vec3d vec3d = new Vec3d(0, 0, freecam.getSpeed() * 2.5).rotateY((float) -Math.toRadians(this.headYaw));
-        Vec3d strafe = vec3d.rotateY((float) Math.toRadians(90));
+        Vec3d forward = new Vec3d(0, 0, this.freecam.getSpeed() * 2.5).rotateY((float) -Math.toRadians(this.headYaw));
+        Vec3d strafe = forward.rotateY((float) Math.toRadians(90));
         Vec3d motion = this.getVelocity();
 
         motion = motion.add(0, 2 * upDown, 0);
-        motion = motion.add(strafe.getX() * input.movementSideways, 0, strafe.getZ() * input.movementSideways);
-        motion = motion.add(vec3d.getX() * input.movementForward, 0, vec3d.getZ() * input.movementForward);
+        motion = motion.add(strafe.getX() * this.input.movementSideways, 0, strafe.getZ() * this.input.movementSideways);
+        motion = motion.add(forward.getX() * this.input.movementForward, 0, forward.getZ() * this.input.movementForward);
 
         this.setPos(this.getX() + motion.getX(), this.getY() + motion.getY(), this.getZ() + motion.getZ());
     }
