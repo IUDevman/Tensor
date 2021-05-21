78848238d1ec59c9e82e6e42312bce683fbea654
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-05-21 11:14:19 -0500

    Create AutoWalk.java

---
+++ src/main/java/dev/tensor/feature/modules/AutoWalk.java
@@ -0,0 +1,18 @@
+package dev.tensor.feature.modules;
+
+import dev.tensor.misc.imp.Category;
+import dev.tensor.misc.imp.Module;
+
+/**
+ * @author IUDevman
+ * @since 05-21-2021
+ */
+
+@Module.Info(name = "AutoWalk", category = Category.Movement)
+public final class AutoWalk extends Module {
+
+    @Override
+    public void onTick() {
+        getMinecraft().options.keyForward.setPressed(true);
+    }
+}
