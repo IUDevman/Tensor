aadbcc8bc8f5a7388f1a47529a6268b3fdc49c17
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-05-22 02:52:00 -0500

    Added toggle timer to flight
    On a lot of servers in which you can still fly, the mojang-default "kicked for flying" is enabled. Extended periods of time while flying kicks the player and this bypasses it by toggling it at an interval thus dropping y level and resetting the check.

--- src/main/java/dev/tensor/feature/modules/Flight.java
+++ src/main/java/dev/tensor/feature/modules/Flight.java
@@ -16,18 +16,32 @@ public final class Flight extends Module {
 
     public final BooleanSetting ignoreFluids = new BooleanSetting("Ignore Fluids", true);
     public final NumberSetting speed = new NumberSetting("Speed", 20, 1, 50, 1);
+    public final BooleanSetting toggleTimer = new BooleanSetting("Toggle Timer", false);
+    public final NumberSetting toggleTicks = new NumberSetting("Toggle Ticks", 10, 1, 40, 0);
+
+    private int delayCount = 0;
+
+    @Override
+    public void onDisable() {
+        delayCount = 0;
+    }
 
     @Override
     public void onTick() {
         getPlayer().abilities.flying = false;
-        getPlayer().flyingSpeed = getSpeed();
-        getPlayer().setVelocity(new Vec3d(0, 0, 0));
 
-        if (getMinecraft().options.keyJump.isPressed()) {
-            getPlayer().setVelocity(getPlayer().getVelocity().add(0, getSpeed(), 0));
-        } else if (getMinecraft().options.keySneak.isPressed()) {
-            getPlayer().setVelocity(getPlayer().getVelocity().add(0, -getSpeed(), 0));
+        if (!toggleTimer.getValue() || delayCount >= toggleTicks.getValue()) {
+            delayCount = 0;
+            getPlayer().flyingSpeed = getSpeed();
+            getPlayer().setVelocity(new Vec3d(0, 0, 0));
+
+            if (getMinecraft().options.keyJump.isPressed()) {
+                getPlayer().setVelocity(getPlayer().getVelocity().add(0, getSpeed(), 0));
+            } else if (getMinecraft().options.keySneak.isPressed()) {
+                getPlayer().setVelocity(getPlayer().getVelocity().add(0, -getSpeed(), 0));
+            }
         }
+        delayCount++;
     }
 
     private float getSpeed() {
