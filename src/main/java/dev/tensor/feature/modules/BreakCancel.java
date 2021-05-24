b99bf76fd5ade3b2a33eccc7de795eb1e8b1e3ce
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-05-23 22:27:37 -0500

    Added BreakCancel

---
+++ src/main/java/dev/tensor/feature/modules/BreakCancel.java
@@ -0,0 +1,14 @@
+package dev.tensor.feature.modules;
+
+import dev.tensor.misc.imp.Category;
+import dev.tensor.misc.imp.Module;
+
+/**
+ * @author IUDevman
+ * @since 05-23-2021
+ */
+
+@Module.Info(name = "BreakCancel", category = Category.Player)
+public final class BreakCancel extends Module {
+
+}
