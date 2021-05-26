f352c63f47c647b42e7ccad3ca44f385b7397231
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-05-26 00:14:21 -0500

    Added NoGlitchBlock

---
+++ src/main/java/dev/tensor/feature/modules/NoGlitchBlock.java
@@ -0,0 +1,14 @@
+package dev.tensor.feature.modules;
+
+import dev.tensor.misc.imp.Category;
+import dev.tensor.misc.imp.Module;
+
+/**
+ * @author IUDevman
+ * @since 05-25-2021
+ */
+
+@Module.Info(name = "NoGlitchBlock", category = Category.Player)
+public final class NoGlitchBlock extends Module {
+
+}
