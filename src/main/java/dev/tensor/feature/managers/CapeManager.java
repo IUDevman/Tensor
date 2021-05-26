e285a27b600e734fb7eca7aabbb60a5cc9d2e913
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-05-26 16:57:43 -0500

    Finished capes

--- src/main/java/dev/tensor/feature/managers/CapeManager.java
+++ src/main/java/dev/tensor/feature/managers/CapeManager.java
@@ -2,10 +2,16 @@ package dev.tensor.feature.managers;
 
 import dev.tensor.Tensor;
 import dev.tensor.misc.imp.Manager;
+import net.minecraft.client.texture.NativeImage;
+import net.minecraft.client.texture.NativeImageBackedTexture;
+import net.minecraft.util.Identifier;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -23,6 +29,7 @@ public enum CapeManager implements Manager {
     INSTANCE;
 
     private final ArrayList<String> capes = new ArrayList<>();
+    private Identifier cape = null;
 
     @Override
     public void load() {
@@ -62,5 +69,31 @@ public enum CapeManager implements Manager {
 
     public boolean hasCape(String name) {
         return this.capes.contains(name);
+    }
+
+    public Identifier getCape(String name) {
+        if (isNull()) return null;
+
+        if (this.capes.contains(name) || getPlayer().getEntityName().equalsIgnoreCase(name)) {
+            try {
+                if (this.cape == null) {
+                    Path path = Paths.get(Tensor.INSTANCE.MOD_NAME.toLowerCase(Locale.ROOT) + "/Cape.png");
+
+                    if (!Files.exists(path)) return null;
+
+                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                    ImageIO.write(ImageIO.read(path.toFile()), "png", byteArrayOutputStream);
+                    InputStream inputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+
+                    this.cape = getMinecraft().getTextureManager().registerDynamicTexture("666cape666", new NativeImageBackedTexture(NativeImage.read(inputStream)));
+                }
+            } catch (IOException ignored) {
+                Tensor.INSTANCE.LOGGER.info("Failed to load cape!");
+            }
+
+            return this.cape;
+        }
+
+        return null;
     }
 }
