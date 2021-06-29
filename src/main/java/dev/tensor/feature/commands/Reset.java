4ab08090f99fe61667a0279d2f01c93c7647ec83
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-06-29 07:18:06 -0500

    Create Reset.java

---
+++ src/main/java/dev/tensor/feature/commands/Reset.java
@@ -0,0 +1,72 @@
+package dev.tensor.feature.commands;
+
+import dev.tensor.feature.managers.ModuleManager;
+import dev.tensor.feature.managers.SettingManager;
+import dev.tensor.misc.imp.Command;
+import dev.tensor.misc.imp.Module;
+import dev.tensor.misc.imp.Setting;
+import net.minecraft.util.Formatting;
+import org.lwjgl.glfw.GLFW;
+
+/**
+ * @author IUDevman
+ * @since 06-29-2021
+ */
+
+public final class Reset implements Command {
+
+    @Override
+    public String getName() {
+        return "Reset";
+    }
+
+    @Override
+    public String getMarker() {
+        return "(" + Formatting.YELLOW + this.getName() + Formatting.GRAY + ") ";
+    }
+
+    @Override
+    public String getSyntax() {
+        return "{alias} [module]";
+    }
+
+    @Override
+    public String[] getAliases() {
+        return new String[]{
+                "reset",
+                "default",
+                "clear"
+        };
+    }
+
+    @Override
+    public int getID() {
+        return 681;
+    }
+
+    @Override
+    public void onCommand(String[] message) {
+        if (message == null || message.length < 2) {
+            this.sendReplaceableClientMessage(this.getMarker() + "No module inputted!", this.getID(), true);
+            return;
+        }
+
+        String moduleName = message[1];
+
+        Module module = ModuleManager.INSTANCE.getModule(moduleName);
+
+        if (module == null) {
+            this.sendReplaceableClientMessage(this.getMarker() + "Invalid module (" + Formatting.YELLOW + moduleName + Formatting.GRAY + ")!", this.getID(), true);
+            return;
+        }
+
+        module.setDrawn(true);
+        module.setMessages(false);
+        module.setEnabled(false);
+        module.setBind(GLFW.GLFW_KEY_UNKNOWN);
+
+        SettingManager.INSTANCE.getSettingsForModule(module).forEach(Setting::reset);
+
+        this.sendReplaceableClientMessage(this.getMarker() + "Reset: " + module.getName() + "!", this.getID(), true);
+    }
+}
