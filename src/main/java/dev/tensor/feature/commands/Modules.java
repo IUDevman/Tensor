0d9874e54065a06f0fd0afc138df20c34b64fc1a
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-07-04 16:33:46 -0500

    Create Modules.java

---
+++ src/main/java/dev/tensor/feature/commands/Modules.java
@@ -0,0 +1,63 @@
+package dev.tensor.feature.commands;
+
+import dev.tensor.Tensor;
+import dev.tensor.misc.imp.Category;
+import dev.tensor.misc.imp.Command;
+import net.minecraft.util.Formatting;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * @author IUDevman
+ * @since 07-04-2021
+ */
+
+public final class Modules implements Command {
+
+    @Override
+    public String getName() {
+        return "Modules";
+    }
+
+    @Override
+    public String getMarker() {
+        return "(" + Formatting.YELLOW + this.getName() + Formatting.GRAY + ") ";
+    }
+
+    @Override
+    public String getSyntax() {
+        return "{alias}";
+    }
+
+    @Override
+    public String[] getAliases() {
+        return new String[]{
+                "modules",
+                "modulelist"
+        };
+    }
+
+    @Override
+    public int getID() {
+        return 683;
+    }
+
+    @Override
+    public void onCommand(String[] message) {
+        this.sendClientMessage(this.getMarker() + "Available modules:", true);
+
+        Arrays.stream(Category.values()).forEach(category -> {
+
+            ArrayList<String> modules = new ArrayList<>();
+
+            Tensor.INSTANCE.MODULE_MANAGER.getModulesInCategory(category).forEach(module -> modules.add((module.isEnabled() ? Formatting.GREEN : "") + module.getName() + Formatting.GRAY));
+
+            int count = modules.size();
+
+            String moduleString = modules.toString().replace("[", "").replace("]", "");
+
+            this.sendClientMessage(Formatting.YELLOW + category.name() + Formatting.GRAY + " (" + Formatting.YELLOW + count + Formatting.GRAY + "): " + moduleString, true);
+        });
+    }
+}
