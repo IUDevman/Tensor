f8636c031d0b2ce8beeb22b98866999297a32a7a
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-07-18 03:32:21 -0500

    Update Flight.java

--- src/main/java/dev/tensor/feature/modules/Flight.java
+++ src/main/java/dev/tensor/feature/modules/Flight.java
@@ -17,15 +17,6 @@ public final class Flight extends Module {
     public final BooleanSetting ignoreFluids = new BooleanSetting("Ignore Fluids", true);
     public final BooleanSetting elytraCheck = new BooleanSetting("Elytra Check", true);
     public final NumberSetting speed = new NumberSetting("Speed", 20, 1, 50, 1);
-    public final BooleanSetting toggleTimer = new BooleanSetting("Toggle Timer", false);
-    public final NumberSetting toggleTicks = new NumberSetting("Toggle Ticks", 10, 1, 40, 0);
-
-    private int delayCount = 0;
-
-    @Override
-    public void onDisable() {
-        delayCount = 0;
-    }
 
     @Override
     public void onTick() {
@@ -33,18 +24,14 @@ public final class Flight extends Module {
 
         this.getPlayer().abilities.flying = false;
 
-        if (!toggleTimer.getValue() || delayCount >= toggleTicks.getValue()) {
-            delayCount = 0;
-            this.getPlayer().flyingSpeed = getSpeed();
-            this.getPlayer().setVelocity(new Vec3d(0, 0, 0));
+        this.getPlayer().flyingSpeed = getSpeed();
+        this.getPlayer().setVelocity(new Vec3d(0, 0, 0));
 
-            if (this.getMinecraft().options.keyJump.isPressed()) {
-                this.getPlayer().setVelocity(this.getPlayer().getVelocity().add(0, getSpeed(), 0));
-            } else if (this.getMinecraft().options.keySneak.isPressed()) {
-                this.getPlayer().setVelocity(this.getPlayer().getVelocity().add(0, -getSpeed(), 0));
-            }
+        if (this.getMinecraft().options.keyJump.isPressed()) {
+            this.getPlayer().setVelocity(this.getPlayer().getVelocity().add(0, getSpeed(), 0));
+        } else if (this.getMinecraft().options.keySneak.isPressed()) {
+            this.getPlayer().setVelocity(this.getPlayer().getVelocity().add(0, -getSpeed(), 0));
         }
-        delayCount++;
     }
 
     private float getSpeed() {
