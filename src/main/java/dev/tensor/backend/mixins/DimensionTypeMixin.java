5e59ae30748ac67f8bffe806e18906732d17280d
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-07-21 23:21:24 -0500

    Changed fullbright mixin
    Should fix the issue with smooth lighting.

--- src/main/java/dev/tensor/backend/mixins/DimensionTypeMixin.java
+++ src/main/java/dev/tensor/backend/mixins/DimensionTypeMixin.java
@@ -3,23 +3,22 @@ package dev.tensor.backend.mixins;
 import dev.tensor.Tensor;
 import dev.tensor.feature.modules.FullBright;
 import dev.tensor.misc.imp.Global;
-import net.minecraft.client.render.LightmapTextureManager;
-import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
- * @author IUDevman
- * @since 05-12-2021
+ * @author A2H#6255
+ * Added with explicit permission, thanks! :D
  */
 
-@Mixin(value = LightmapTextureManager.class, priority = Integer.MAX_VALUE)
-public final class LightmapTextureManagerMixin implements Global {
+@Mixin(value = DimensionType.class, priority = Integer.MAX_VALUE)
+public final class DimensionTypeMixin implements Global {
 
-    @Inject(method = "getBrightness", at = @At("HEAD"), cancellable = true)
-    public void getBrightness(World world, int i, CallbackInfoReturnable<Float> cir) {
+    @Inject(method = "method_28516", at = @At("HEAD"), cancellable = true)
+    public void method_28516(int i, CallbackInfoReturnable<Float> cir) {
         FullBright fullBright = Tensor.INSTANCE.MODULE_MANAGER.getModule(FullBright.class);
 
         if (fullBright != null && fullBright.isEnabled()) {
