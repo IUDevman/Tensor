d19cceac7c7f827adb8b9cf4251bdb9774ba6ce2
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-07-29 02:58:23 -0500

    Update PowderSnowBlockMixin.java

--- src/main/java/dev/tensor/backend/mixins/PowderSnowBlockMixin.java
+++ src/main/java/dev/tensor/backend/mixins/PowderSnowBlockMixin.java
@@ -4,9 +4,15 @@ import dev.tensor.Tensor;
 import dev.tensor.backend.MixinPriority;
 import dev.tensor.feature.modules.Jesus;
 import dev.tensor.misc.imp.Global;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.PowderSnowBlock;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -29,12 +35,19 @@ public final class PowderSnowBlockMixin implements Global {
         Jesus jesus = Tensor.INSTANCE.MODULE_MANAGER.getModule(Jesus.class);
 
         if (jesus != null && jesus.isEnabled() && jesus.powderedSnow.getValue()) {
-
-            if (jesus.cancelOnFall.getValue() && minecraft.player.fallDistance >= 3) {
-                return;
-            }
-
             cir.setReturnValue(true);
+        }
+    }
+
+    @Inject(method = "getCollisionShape", at = @At("HEAD"), cancellable = true)
+    public void getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context, CallbackInfoReturnable<VoxelShape> cir) {
+        if (this.isNull() || this.getPlayer().inPowderSnow) return;
+
+        Jesus jesus = Tensor.INSTANCE.MODULE_MANAGER.getModule(Jesus.class);
+
+        if (jesus != null && jesus.isEnabled() && jesus.powderedSnow.getValue() && jesus.cancelOnFall.getValue() && this.getPlayer().fallDistance >= 3) {
+
+            cir.setReturnValue(VoxelShapes.fullCube());
         }
     }
 }
