f46e774ff1e32a3a9974a9fd2169b2f4ca78cb9e
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-08-10 19:55:54 -0500

    0.8.0-RELEASE

--- src/main/java/dev/tensor/Tensor.java
+++ src/main/java/dev/tensor/Tensor.java
@@ -21,7 +21,7 @@ public final class Tensor implements ModInitializer {
     }
 
     public final String MOD_NAME = "Tensor";
-    public final String MOD_VERSION = "0.8.0-SNAPSHOT";
+    public final String MOD_VERSION = "0.8.0-RELEASE";
 
     public final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 
