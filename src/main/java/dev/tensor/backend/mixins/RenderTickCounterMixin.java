0e22f5efe7705fd49e5a8ac411810120897107b7
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-09-10 22:18:42 -0500

    Update RenderTickCounterMixin.java

--- src/main/java/dev/tensor/backend/mixins/RenderTickCounterMixin.java
+++ src/main/java/dev/tensor/backend/mixins/RenderTickCounterMixin.java
@@ -5,7 +5,6 @@ import dev.tensor.backend.MixinPriority;
 import dev.tensor.feature.modules.Timer;
 import dev.tensor.misc.imp.Global;
 import net.minecraft.client.render.RenderTickCounter;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,7 +22,7 @@ public final class RenderTickCounterMixin implements Global {
     @Shadow
     public float lastFrameDuration;
 
-    @Inject(method = "beginRenderTick", at = @At(value = "FIELD", target = "Lnet/minecraft/client/render/RenderTickCounter;prevTimeMillis:J", opcode = Opcodes.PUTFIELD))
+    @Inject(method = "beginRenderTick", at = @At(value = "FIELD", target = "Lnet/minecraft/client/render/RenderTickCounter;prevTimeMillis:J"))
     public void beginRenderTick(long timeMillis, CallbackInfoReturnable<Integer> cir) {
         Timer timer = Tensor.INSTANCE.MODULE_MANAGER.getModule(Timer.class);
 
